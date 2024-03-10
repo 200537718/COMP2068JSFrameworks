fa7c724ae0a7964fa90c5441ef1045d750e055ec
Jaspreet Kaur <123716853+200537718@users.noreply.github.com>
2024-03-09 20:33:54 -0500

    calculator

--- COMP2068JSFrameworks/midterm/home.js
+++ COMP2068JSFrameworks/midterm/home.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const mongoose =require('mongoose');
-mongoose.connect(process.env.DB_CONNECTION_STRING,{USEnEWPARESER: TRUE,useUnifiedTopology:true})
-.then(())=console.log('connected to database'))
-.catch(err =>console.error('error connecting to database:',err));
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to database'))
+  .catch(err => console.error('Error connecting to database:', err));
